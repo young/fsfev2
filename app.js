11dcd440f24119c8c9ac5c6a44db709108827fe0
Jem <jem@netflix.com>
2019-10-03 20:40:49 +0000

    WIP

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Nice work, Jem!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
