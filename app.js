0341c8103324d88c89a032eb9b66471d7a7722b5
young <jem@netflix.com>
2019-10-04 11:38:11 -0500

    Add bot

--- app.js
+++ app.js
@@ -1,10 +1,92 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path');
+const app = express();
 
-app.get('/', (req, res) => res.send('Nice work, Jem!'))
+const WebSocketServer = require('ws').Server;
+const server = require('http').createServer(app);
+const wss = new WebSocketServer({ server });
+const port = 3000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Serve js files
+app.use('/js', express.static(path.join(__dirname, 'ui/js/')));
+// Serve css files
+app.use('/css', express.static(path.join(__dirname, 'ui/css/')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/ui/html/index.html'));
+});
 
 
 
+function handleQuery(query, cb) {
+    cb('Awesome');
+}
+
+/**
+ * Client Counter
+ * Count the number of active connections
+ * @type {Number}
+ */
+let cc = 0;
+
+wss.on('connection', function connection(ws) {
+   console.log('client connections: ', ++cc);
+
+  ws.on('message', function incoming(message) {
+    try {
+        const { payload, type } = JSON.parse(message);
+        switch(type) {
+            case 'query':
+                handleQuery(payload, (response) => {
+                    ws.send(JSON.stringify({type: 'queryResponse', payload: response}));
+                });
+                return;
+            default:
+                console.log(message);
+        }
+
+    } catch(e) {
+      console.error('Error from message: ', e);
+    }
+  });
+
+  // Send welcome message on each connection
+  if (ws.readyState === ws.OPEN) {
+    ws.send(JSON.stringify({type: 'connected', payload: 'Welcome!'}));
+  }
+
+  ws.on('close', function close() {
+    --cc;
+    if (cc === 0) {
+      clearInterval(pingInterval);
+    }
+    console.log('disconnected');
+  });
+
+  ws.on('error', function error() {
+    --cc;
+    console.log('error');
+  });
+
+});
+
+const pingPayload = JSON.stringify({type: 'ping'});
+// Keep the connection alive
+let pingInterval = setInterval(() => {
+  wss.broadcast(pingPayload);
+}, 1 * 100);
+
+/**
+ * Broadcast data to all connected clients
+ * @param  {Object} data
+ * @void
+ */
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
+
+
+server.listen(port, () => console.log(`App listening on port ${port}!`))
---
+++ ui/js/index.js
@@ -0,0 +1,47 @@
+const webSocketOutput = document.querySelector('.output');
+const inputBtn = document.querySelector('.input-box');
+
+inputBtn.addEventListener('keyup', function(event) {
+    if (event.key === "Enter") {
+        const query = event.target.value;
+        event.target.value = '';
+        sendQuery(query);
+    }
+});
+
+let ws;
+try {
+  // Local dev
+  if (['localhost', '127.0.0.1', ''].includes(location.hostname)) {
+    ws = new WebSocket(`ws://localhost:3000`);
+  } else {
+    ws = new WebSocket(`wss://jemisthe.best`);
+  }
+} catch(e) {
+  console.log('Web socket init error', e);
+}
+
+
+function sendQuery(query) {
+    ws.send(JSON.stringify({type: 'query', payload: query}))
+}
+
+ws.onmessage = function({data}) {
+    const msg = document.createElement('div');
+
+    try {
+        data = JSON.parse(data);
+        const {type, payload} = data;
+
+        switch(type) {
+            // case 'ping':
+            //     // console.log('ping');
+            //     return;
+            default:
+                msg.innerHTML = payload;
+                webSocketOutput.prepend(msg);
+        }
+    } catch(e) {
+        console.log('Websocket error', e);
+    }
+}
